3af7302bffa153275c81fd1732d5427e153860ff
fcsouza <fcsouza18@hotmail.com>
2020-08-10 20:21:47 -0300

    refactor landingPage

--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -5,7 +5,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#8257E5',
     justifyContent: 'center',
-    padding: 40
+    padding: 40,
   },
 
   banner: {
@@ -18,34 +18,34 @@ const styles = StyleSheet.create({
     color: '#FFF',
     fontSize: 20,
     lineHeight: 30,
-    marginTop: 80
+    marginTop: 40, // antes 80
   },
 
   titleBold: {
     fontFamily: 'Poppins_600SemiBold',
   },
-  
-  buttonsContainer: {
+
+  butttonsContainer: {
     flexDirection: 'row',
     marginTop: 40,
     justifyContent: 'space-between',
   },
-  
+
   button: {
     height: 150,
     width: '48%',
     backgroundColor: '#333',
     borderRadius: 8,
     padding: 24,
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
 
   buttonPrimary: {
-    backgroundColor: '#9871F5',
+    backgroundColor: '#9871f5',
   },
 
   buttonSecondary: {
-    backgroundColor: '#04D361',
+    backgroundColor: '#04d361',
   },
 
   buttonText: {
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     color: '#d4c2ff',
     fontSize: 12,
     lineHeight: 20,
-    maxWidth: 140,
+    maxHeight: 140,
     marginTop: 40,
   }
 });
